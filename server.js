84210a5f6a885ae229300beb0821b2a093e0815f
katie <katie>
2016-07-12 18:40:11 +0800

    angular route demo

---
+++ server.js
@@ -0,0 +1,36 @@
+/*var http = require('http'),
+	fs = require('fs');
+
+fs.readFile('./app/index.html', function(err, html) {
+	if(err) {
+		throw err;
+	}
+	http.createServer(function(req, res) {
+		res.writeHeader(200, {"Content-Type": "text/html"});
+		res.write(html);
+		res.end();
+	}).listen(3030);
+	console.log('HTTP server is listening at port 3030.');
+});*/
+
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+app.use('/libs', express.static(__dirname + '/'));
+
+
+fs.readFile('./index.html', function(err, html) {
+	if(err) {
+		throw err;
+	}
+	app
+		.get('/', function(req, res) {
+			res.writeHeader(200, {"Content-Type": "text/html"});
+			res.write(html);
+			res.end();
+		})
+		.listen(3032, function() {
+			console.log('Example app listening on port 3031.')
+		});
+});
